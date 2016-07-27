74876c1080f8425b0b7c324fed2be74910c1d62c
AbhishekPalsule <abhishek.palsule@gmail.com>
2016-07-27 15:09:16 +0530

    Add files via upload

--- Diamond.java
+++ Diamond.java
@@ -1,40 +1,73 @@
-/**
+package mastermind; /**
  * Created by dullus on 7/27/2016.
  */
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Diamond {
-    public static void main(String[] args) throws IOException
-    {
-        ArrayList<String> words = playDiamond(3);
-        for(String i : words)
-        {
-            System.out.println(i);
-        }
-    }
-    public static ArrayList<String> playDiamond(int level)
-    {
-        ArrayList<String> dictionary = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        int difficulty = 3;
+        Scanner Scan = new Scanner(System.in);
+        ArrayList<String> dictionary = getWords(difficulty);
+        HashSet<Character> finalLetter = new HashSet<>();
+        HashSet<Character> possibleLetters = new HashSet<>();
+        String hiddenWord = dictionary.get((int)(Math.random()*dictionary.size()));
+        boolean Compwon =false;
 
-
-        if( level == 3) {
-            try {
-                FileReader fileReader = new FileReader("C:\\Users\\dullus\\Documents\\Books\\sowpods.txt");
-                BufferedReader bufferReader = new BufferedReader(fileReader);
-                String line = "";
-                while ((line = bufferReader.readLine()) != null) {
-                    String word = line;
-                    if(word.length() == 3 )
-                        dictionary.add(word);
-                }
-
-
-            }catch (Exception e) {
-
+        while(!Compwon){
+            String currentWord = selectWords(dictionary);
+            int matches = Scan.nextInt();
+            if( matches > 0 ){
+                //char character = oneChar()
             }
         }
-        return dictionary;
+    }
+
+    public static ArrayList<String> getWords(int length) {
+        ArrayList<String> Words = new ArrayList<String>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\palsulea\\IdeaProjects\\Test\\src\\sowpods.txt"));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                String[] splitted = line.split("\\n");
+                for(String w:splitted)
+                    Words.add(w);
+            }
+        }
+
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Words;
+    }
+
+    public static char oneChar(Set<Character> possibleChars , String word){
+        char letters[] = word.toCharArray();
+        char selectedChar = ' ';
+        for(char letter : letters){
+        if(possibleChars.contains(letter)){
+            possibleChars.remove(letter);
+            selectedChar = letter;
+        }else{
+            possibleChars.add(letter);
+        }
+        }
+        return selectedChar;
+    }
+
+    public static String selectWords(ArrayList<String> dictionary){
+        String word = dictionary.get(0);
+        dictionary.remove(0);
+        return word;
     }
 }
+
+
